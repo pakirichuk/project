22e0fdf59934abb76ffa4fc27456397edddb24dc
Petr <petr.kiri4uk@yandex.ru>
2022-01-24 15:35:53 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+
+//console.log(5 + + "5");
+
+let incr = 10,
+    decr = 10;
+
+//incr++;
+//decr--;
+
+console.log(++incr);
+console.log(--decr);
+
+
+console.log(5%2);
+
+console.log(2 + 2 *2 != '6');
+
+
+const isChaked =false,
+      isClose =false;
+
+console.log(isChaked || !isClose);
